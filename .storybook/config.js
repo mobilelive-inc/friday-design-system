40bbd86e18dcb074b34136a2db08b2a346b8d172
samaid <samaid.saeed@mobilelive.ca>
2020-06-04 16:32:09 +0500

    storybook configured

---
+++ .storybook/config.js
@@ -0,0 +1,22 @@
+// import { setIntlConfig, withIntl } from 'storybook-addon-intl';
+// import { addLocaleData } from 'react-intl';
+// import enLocaleData from 'react-intl/locale-data/en';
+// import frLocaleData from 'react-intl/locale-data/fr';
+
+// addLocaleData(enLocaleData);
+// addLocaleData(frLocaleData);
+
+// const messages = {
+//   'en': "",
+//   'fr': ""
+//   }
+
+// const getMessages = (locale) => messages[locale];
+
+// setIntlConfig({
+//   locales: ['en', 'fr'],       
+//   defaultLocale: 'en',
+//     getMessages
+//    });
+
+//    addDecorator(withIntl);
