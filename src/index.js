d838d35feaac64bc9314cec62d62623c9e309866
samaid <samaid.saeed@mobilelive.ca>
2020-06-04 18:49:07 +0500

    minor change in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import DesignSystem from './app'
 
 function App(){
-    return(
-        <DesignSystem />
-    )
+  return(
+    <h1>Design System</h1>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
