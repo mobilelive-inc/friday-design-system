6203f9d259b62318da0489e11f08d8ad6a62b5ae
samaid <samaid.saeed@mobilelive.ca>
2020-06-22 20:58:46 +0500

    feat: typography story added

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import  Button from '../src/components/Button/';
+import  Button from "../src/components/Button";
 
 export default {
   title: 'Button',
