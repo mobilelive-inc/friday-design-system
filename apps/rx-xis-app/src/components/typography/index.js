f01942a80ae0b3c8feaa94e39ac8b788b951eb90
samaid <samaid.saeed@mobilelive.ca>
2020-07-13 17:05:25 +0500

    feature/button-documentation

--- apps/rx-xis-app/src/components/typography/index.js
+++ apps/rx-xis-app/src/components/typography/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import {
@@ -11,7 +12,7 @@ import {
 } from 'styled-system';
 import { Text as RebassText } from 'rebass/styled-components';
 
-export const Text = styled(RebassText)(
+const BaseText = styled(RebassText)(
   compose(
     border,
     color,
@@ -22,9 +23,62 @@ export const Text = styled(RebassText)(
   ),
 );
 
+export const Text = (props) => {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <BaseText {...props}/>
+  )
+}
+
 Text.propTypes = {
+  /** Text for Text could be string or node. */
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
-  ])
+  ]),
+  /** Typography variant consumes typography object in the theme file */
+  variant: PropTypes.string,
+  /** Defines color of text. Property maps to colors object in theme */
+  color: PropTypes.string,
+  /** Defines font size of child elements. Accepts responsive value from theme */
+  fontSize: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+    PropTypes.number,
+    PropTypes.arrayOf(PropTypes.number),
+  ]),
+  /** Defines line height of child elements. Accepts responsive value from theme */
+  lineHeight: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+    PropTypes.number,
+    PropTypes.arrayOf(PropTypes.number),
+  ]),
+  /** Defines font weight of child elements. Accepts responsive value from theme */
+  fontWeight: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+    PropTypes.number,
+    PropTypes.arrayOf(PropTypes.number),
+  ]),
+  /** Shorthand to add margin to button. ml, mr, mt, mb, my and mx are also supported to
+   * to add margins on left, right, top, bottom, y-axis and x-axis respectively.
+   * Accepts responsive value from theme
+   */
+  m: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+    PropTypes.number,
+    PropTypes.arrayOf(PropTypes.number),
+  ]),
+  /** Shorthand to add padding to button. pl, pr, pt, pb, py and px are also supported to
+   * to add paddings on left, right, top, bottom, y-axis and x-axis respectively.
+   * Accepts responsive value from theme
+   */
+  p: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+    PropTypes.number,
+    PropTypes.arrayOf(PropTypes.number),
+  ]),
 };
--- apps/rx-xis-app/stories/typography.stories.js
+++ apps/rx-xis-app/stories/typography.stories.js
@@ -10,12 +10,12 @@ export default {
 
 export const Typography = () => (
   <ThemeProvider theme={theme}>
-    <Text variant="H1"> Heading 1 </Text>
-    <Text mt="20px" variant="H2"> Heading 2 </Text>
-    <Text mt="20px" variant="H3">Heading 3</Text>
-    <Text mt="20px" variant="H4">Heading 4</Text>
-    <Text mt="20px" variant="H5">Heading 5</Text>
-    <Text mt="20px" variant="H6">Heading 5</Text>
-    <Text mt="20px">This is a paragraph</Text>
+    <Text variant="H1"> <span>Heading 1</span> </Text>
+    <Text mt="20px" color="primary" variant="H2"> Heading 2 </Text>
+    <Text mt="20px" color="secondary" variant="H3">Heading 3</Text>
+    <Text mt="20px" color="success" variant="H4">Heading 4</Text>
+    <Text mt="20px" color="danger" variant="H5">Heading 5</Text>
+    <Text mt="20px" color="blue" variant="H6">Heading 5</Text>
+    <Text mt="20px" color="green">This is a paragraph</Text>
   </ThemeProvider>
 )
