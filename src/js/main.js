98b59579e9bdf211ffb8b781da4b25f6ed9c7a59
Mutahar Aslam <mutahar.aslam@mobilelive.ca>
2020-07-14 22:16:29 +0500

    modified

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,1 @@
 import "./themeSwitcher";
-
-// function switchDarkTheme() {
-//   var element = document.getElementsByClassName("wrapper");
-//   element.classList.add("theme-dark");
-// }
--- src/js/module.js
+++ src/js/module.js
@@ -1,4 +1,1 @@
 // This will get imported in main.js
-export const logSomething = (msg) => {
-  console.log(msg);
-};
