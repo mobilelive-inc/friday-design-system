65997e825094ba693ae7bc7fb8b4ad8a35439d52
samaid <samaid.saeed@mobilelive.ca>
2020-08-13 17:29:07 +0500

    implemented with tippy

--- apps/rx-xis-app/src/components/tooltip/index.js
+++ apps/rx-xis-app/src/components/tooltip/index.js
@@ -1,52 +1,78 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TooltipWrapper, ToolTip } from './css';
+// import { TooltipWrapper, ToolTip } from './css';
 import {useSpring, animated} from 'react-spring';
+import {
+  Tooltip as BaseTooltip,
+} from 'react-tippy';
+import 'react-tippy/dist/tippy.css';
+// import './css.css'
 
 const Tooltip = ({
   position,
-  withArrow,
-  curved,
+  arrow,
+  arrowSize,
+  animation,
   backgroundColor,
   color,
-  text,
+  children,
+  delay,
+  distance,
+  hideDelay,
+  labelText,
+  tabIndex,
+  theme,
+  size,
   ...restProps
 }) => {
+  React.useEffect(() => {
+    if(theme === 'light'){
+      require('./css.css');
+    }
+  },[])
   return (
-      <ToolTip
-      data-arrow={withArrow}
-      withArrow = {!withArrow}
-      data-position={position}
-      tabindex="1"
-      backgroundColor={backgroundColor}
-      color={color}
-      data-tooltip
-      tabindex="1"
-      curved={curved}
-      // mb={mb}
-      // marginBottom={marginBottom}
-      {...restProps}
-      >
-      {text}
-      </ToolTip
+    <BaseTooltip
+    title={labelText}
+    position={position}
+    arrow={arrow}
+    arrowSize={arrowSize}
+    delay={delay}
+    distance={distance}
+    hideDelay={hideDelay}
+    size={size}
+    interactive
+    trigger="mouseenter focus"
+    // trigger="click"
+    tabIndex={tabIndex}
+    // theme={theme}
     >
+    {children}
+  </BaseTooltip>
   )
 }
 
 Tooltip.propTypes = {
-  position: PropTypes.oneOf(['top','top-left','top-right','bottom','bottom-left','bottom-right',
+  position: PropTypes.oneOf(['top','top-start','top-end','bottom','bottom-start','bottom-end',
   'left','left-start','left-end','right','right-start','right-end']),
-  withArrow: PropTypes.bool,
+  arrow: PropTypes.bool,
+  arrowSize: PropTypes.oneOf(['small', 'regular', 'big']),
+  animation: PropTypes.oneOf(['shift', 'perspective', 'fade', 'scale', 'none']),
   backgroundColor: PropTypes.string,
   color: PropTypes.string,
-  text: PropTypes.string,
-  mb:"1rem"
+  distance: PropTypes.number,
+  labelText: PropTypes.string,
+  delay: PropTypes.string,
+  hideDelay: PropTypes.string,
+  tabIndex: PropTypes.number,
+  theme: PropTypes.oneOf(['light','dark','transparent'])
 }
-ToolTip.defaultProps={
-  backgroundColor:'black',
-  color:'white',
+Tooltip.defaultProps={
+  backgroundColor:'',
+  color:'',
   position:'bottom',
-  withArrow: true
+  arrow: false,
+  arrowSize: 'regular',
+  theme: 'dark'
 }
 
 export default Tooltip;
