92dd0c603ff883f5f60b30391cb70de421e218a6
Dmitriy Korol <dmytro.korol@mobilelive.ca>
2020-11-12 06:07:36 -0500

    fix react storybook theming

--- apps/rx-xis-app/.storybook/manager.js
+++ apps/rx-xis-app/.storybook/manager.js
@@ -1,6 +1,7 @@
 import { addons } from '@storybook/addons';
 import { themes } from '@storybook/theming';
+import theme from '../../../.storybook/defaultTheme';
 
 addons.setConfig({
-  theme: themes.dark,
+  theme,
 });
