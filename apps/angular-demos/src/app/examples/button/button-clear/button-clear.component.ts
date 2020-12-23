598f64af349b2485ae3b84cb9646e91d493a09a1
Farrukh Subhani <farrukh.subhani@mobilelive.ca>
2020-12-23 20:58:35 +0500

    button classes and spacing issue fixed

--- apps/angular-demos/src/app/examples/button/button-clear/button-clear.component.ts
+++ apps/angular-demos/src/app/examples/button/button-clear/button-clear.component.ts
@@ -3,24 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'fds-ml-button-clear',
   template: `
-    <button type="button" class="clear btn btn__primary">Primary</button>
-    <button type="button" class="clear btn btn__secondary">Secondary</button>
-    <button type="button" class="clear btn btn__success">Success</button>
-    <button type="button" class="clear btn btn__danger">Danger</button>
-    <button type="button" class="clear btn btn__warning">Warning</button>
-    <button type="button" class="clear btn btn__info">Info</button>
-    <button type="button" class="clear btn btn__light">Light</button>
-    <button type="button" class="clear btn btn__dark">Dark</button>
-    <button type="button" class="clear btn btn__dark disabled">Disabled</button>
+    <button type="button" class="clear btn btn__primary mb-tiny">
+      Primary
+    </button>
+    <button type="button" class="clear btn btn__secondary mb-tiny">
+      Secondary
+    </button>
+    <button type="button" class="clear btn btn__success mb-tiny">
+      Success
+    </button>
+    <button type="button" class="clear btn btn__danger mb-tiny">Danger</button>
+    <button type="button" class="clear btn btn__warning mb-tiny">
+      Warning
+    </button>
+    <button type="button" class="clear btn btn__info mb-tiny">Info</button>
+    <button type="button" class="clear btn btn__light mb-tiny">Light</button>
+    <button type="button" class="clear btn btn__dark mb-tiny">Dark</button>
+    <button type="button" class="clear btn btn__dark mb-tiny disabled">
+      Disabled
+    </button>
   `,
-  styles: [
-  ],
+  styles: []
 })
 export class ButtonClearComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- apps/angular-demos/src/app/examples/button/button-curved/button-curved.component.ts
+++ apps/angular-demos/src/app/examples/button/button-curved/button-curved.component.ts
@@ -3,24 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'demo-button-curved',
   template: `
-    <fds-button className="btn--curved btn__primary">Primary</fds-button>&nbsp;
-    <fds-button className="btn--curved btn__secondary">Secondary</fds-button>&nbsp;
-    <fds-button className="btn--curved btn__success">Success</fds-button>&nbsp;
-    <fds-button className="btn--curved btn__danger">Danger</fds-button>&nbsp;
-    <fds-button className="btn--curved btn__warning">Warning</fds-button>&nbsp;
-    <fds-button className="btn--curved btn__info">Info</fds-button>&nbsp;
-    <fds-button className="btn--curved btn__light">Light</fds-button>&nbsp;
-    <fds-button className="btn--curved btn__dark">Dark</fds-button>&nbsp;
-    <fds-button className="btn--curved btn__dark" [disabled]="true">Disabled</fds-button>
+    <fds-button className="btn__primary curved mb-tiny">Primary</fds-button
+    >&nbsp;
+    <fds-button className="btn__secondary curved mb-tiny">Secondary</fds-button
+    >&nbsp;
+    <fds-button className="btn__success curved mb-tiny">Success</fds-button
+    >&nbsp;
+    <fds-button className="btn__danger curved mb-tiny">Danger</fds-button>&nbsp;
+    <fds-button className="btn__warning curved mb-tiny">Warning</fds-button
+    >&nbsp;
+    <fds-button className="btn__info curved mb-tiny">Info</fds-button>&nbsp;
+    <fds-button className="btn__light curved mb-tiny">Light</fds-button>&nbsp;
+    <fds-button className="btn__dark curved mb-tiny">Dark</fds-button>&nbsp;
+    <fds-button className="btn__dark curved mb-tiny" [disabled]="true"
+      >Disabled</fds-button
+    >
   `,
-  styles: [
-  ],
+  styles: []
 })
 export class ButtonCurvedComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
