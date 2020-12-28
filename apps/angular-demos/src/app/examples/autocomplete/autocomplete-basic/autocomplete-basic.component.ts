7571b78242df861b7bb7b74c96c93dae25af1d5b
Adil Sikandar <adil.sikandar@mobilelive.ca>
2020-12-28 23:36:04 +0500

    I have added angular command & refactor autocomplete code

--- apps/angular-demos/src/app/examples/autocomplete/autocomplete-basic/autocomplete-basic.component.ts
+++ apps/angular-demos/src/app/examples/autocomplete/autocomplete-basic/autocomplete-basic.component.ts
@@ -43,14 +43,8 @@ export class AutocompleteBasicComponent {
   filterCountry(event) {
     //in a real application, make a request to a remote url with the query and return filtered results, for demo we filter at client side
     let filtered: any[] = [];
-    let query = event.query;
-    for (let i = 0; i < this.countries.length; i++) {
-      let country = this.countries[i];
-      if (country.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
-        filtered.push(country);
-      }
-    }
-
+    let { query } = event;
+    this.countries.forEach((item) => item.name.toLowerCase().indexOf(query.toLowerCase()) == 0 && filtered.push(item))
     this.filteredCountries = filtered;
   }
 
