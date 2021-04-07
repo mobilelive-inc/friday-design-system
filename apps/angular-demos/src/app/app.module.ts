909099d1d6d0bf3c05a39d16764c3464eddebbf6
Kashif Ullah <ikashifullah@gmail.com>
2021-04-07 18:53:11 +0500

    Add fds-Table component

--- apps/angular-demos/src/app/app.module.ts
+++ apps/angular-demos/src/app/app.module.ts
@@ -45,6 +45,15 @@ import { MultiCheckboxComponent } from './examples/checkbox/multi/multi.componen
 import { InlineCheckboxComponent } from './examples/checkbox/inline/inline.component';
 import { BasicComponent } from './examples/accordion/basic/basic.component';
 import { MultipleComponent } from './examples/accordion/multiple/multiple.component';
+import { TableSingleSortComponent } from './examples/table/table-single-sort/table-single-sort.component';
+import { TableDarkComponent } from './examples/table/table-dark/table-dark.component';
+import { LightTableHeadComponent } from './examples/table/light-table-head/light-table-head.component';
+import { DarkTableHeadComponent } from './examples/table/dark-table-head/dark-table-head.component';
+import { StripedRowTableComponent } from './examples/table/striped-row-table/striped-row-table.component';
+import { StripedRowEvenTableComponent } from './examples/table/striped-row-even-table/striped-row-even-table.component';
+import { BorderedTableComponent } from './examples/table/bordered-table/bordered-table.component';
+import { HoverableTableComponent } from './examples/table/hoverable-table/hoverable-table.component';
+import { HoverableDarkTableComponent } from './examples/table/hoverable-dark-table/hoverable-dark-table.component';
 
 @NgModule({
   declarations: [
@@ -85,7 +94,16 @@ import { MultipleComponent } from './examples/accordion/multiple/multiple.compon
     MultiCheckboxComponent,
     InlineCheckboxComponent,
     BasicComponent,
-    MultipleComponent
+    MultipleComponent,
+    TableSingleSortComponent,
+    TableDarkComponent,
+    LightTableHeadComponent,
+    DarkTableHeadComponent,
+    StripedRowTableComponent,
+    StripedRowEvenTableComponent,
+    BorderedTableComponent,
+    HoverableTableComponent,
+    HoverableDarkTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ apps/angular-demos/src/app/examples/table/table-single-sort/table-single-sort.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { TableService } from '../table.service';
+
+export interface Task {
+  id?:string;
+  code?:string;
+  name?:string;
+  description?:string;
+  weight?:number;
+  status?:string;
+  category?:string;
+  image?:string;
+  priority?:number;
+  dateCreated?:Date;
+}
+
+@Component({
+  selector: 'demo-table-single-sort',
+  template: `
+    <div>
+    <fds-table [columns]="cols" [value]="tasks"
+      [scrollable]="true" scrollHeight="400px"
+      selectionMode="single" [(selection)]="selectedTask" (onRowSelect)="onRowSelect($event)" dataKey="code"
+      tableStyleClass="fds--table">
+        <ng-template fdsTemplate="caption">
+          <caption class="d--block text--left h4">Dynamic Table with Single Sort, Scrolling, Row Select and Table Styles</caption>
+        </ng-template>
+        <ng-template fdsTemplate="header" let-columns>
+          <tr>
+            <th *ngFor="let col of columns" [fdsSortableColumn]="col.field">
+              {{col.header}} <fds-sortIcon [field]="col.field"></fds-sortIcon>
+            </th>
+            <th>&nbsp;</th>
+          </tr>
+        </ng-template>
+        <ng-template fdsTemplate="body" let-rowData let-columns="columns">
+          <tr [fdsSelectableRow]="rowData">
+            <td *ngFor="let col of columns">
+              {{ col.field ===  'weight'
+                ? (rowData[col.field] | percent)
+                : (col.field ===  'dateCreated'
+                  ?  (rowData[col.field] | date)
+                  : rowData[col.field])
+              }}
+            </td>
+            <td>
+              <fds-button className="btn btn--tooltip text--primary" (onClick)="selectTask(rowData)">Edit</fds-button>
+            </td>
+          </tr>
+        </ng-template>
+      </fds-table>
+    </div>
+  `,
+  styles: [
+  ]
+})
+export class TableSingleSortComponent implements OnInit {
+
+  tasks: Task[];
+  cols: any[];
+  selectedTask: Task;
+
+  constructor(private tableService: TableService) { }
+
+  ngOnInit(): void {
+    this.tableService.getTasks().then(data => this.tasks = data);
+
+    this.cols = [
+        { field: 'code', header: 'Code' },
+        { field: 'name', header: 'Name' },
+        { field: 'category', header: 'Category' },
+        { field: 'weight', header: 'Weight' },
+        { field: 'dateCreated', header: 'DateCreated' }
+    ];
+  }
+
+  selectTask(task: Task) {
+    console.log(task);
+  }
+
+  onRowSelect(event) {
+    console.log(event);
+  }
+
+}
