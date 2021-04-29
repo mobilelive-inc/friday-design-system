b6743efffa626075c6cbe5c85e559b1ff2ccef08
Umer Asif <umer.asif@mobilelive.ca>
2021-04-29 16:15:31 +0500

    improvement

--- libs/angular/src/lib/table/table.service.ts
+++ libs/angular/src/lib/table/table.service.ts
@@ -4,36 +4,35 @@ import { SortMeta } from './table.helpers';
 
 @Injectable()
 export class TableService {
+  private sortSource = new Subject<SortMeta | SortMeta[]>();
+  private selectionSource = new Subject();
+  private valueSource = new Subject<any>();
+  private columnsSource = new Subject();
+  private resetSource = new Subject();
 
-    private sortSource = new Subject<SortMeta|SortMeta[]>();
-    private selectionSource = new Subject();
-    private valueSource = new Subject<any>();
-    private columnsSource = new Subject();
-    private resetSource = new Subject();
+  sortSource$ = this.sortSource.asObservable();
+  selectionSource$ = this.selectionSource.asObservable();
+  valueSource$ = this.valueSource.asObservable();
+  columnsSource$ = this.columnsSource.asObservable();
+  resetSource$ = this.resetSource.asObservable();
 
-    sortSource$ = this.sortSource.asObservable();
-    selectionSource$ = this.selectionSource.asObservable();
-    valueSource$ = this.valueSource.asObservable();
-    columnsSource$ = this.columnsSource.asObservable();
-    resetSource$ = this.resetSource.asObservable();
+  onSort(sortMeta: SortMeta | SortMeta[]) {
+    this.sortSource.next(sortMeta);
+  }
 
-    onSort(sortMeta: SortMeta|SortMeta[]) {
-        this.sortSource.next(sortMeta);
-    }
+  onSelectionChange() {
+    this.selectionSource.next();
+  }
 
-    onSelectionChange() {
-        this.selectionSource.next();
-    }
+  onResetChange() {
+    this.resetSource.next();
+  }
 
-    onResetChange() {
-        this.resetSource.next();
-    }
+  onValueChange(value: any) {
+    this.valueSource.next(value);
+  }
 
-    onValueChange(value: any) {
-        this.valueSource.next(value);
-    }
-
-    onColumnsChange(columns: any[]) {
-        this.columnsSource.next(columns);
-    }
+  onColumnsChange(columns: any[]) {
+    this.columnsSource.next(columns);
+  }
 }
