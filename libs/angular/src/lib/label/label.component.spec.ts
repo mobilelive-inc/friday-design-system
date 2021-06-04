d8c8aecf489982656c3a82d71f2cf35a941a19a1
ahmedbilal1994 <ahmed.bilal@mobilelive.ca>
2021-06-04 19:30:30 +0500

    updated v11

--- libs/angular/src/lib/label/label.component.spec.ts
+++ libs/angular/src/lib/label/label.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LabelComponent } from './label.component';
 
@@ -6,11 +6,13 @@ describe('LabelComponent', () => {
   let component: LabelComponent;
   let fixture: ComponentFixture<LabelComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [LabelComponent]
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [LabelComponent]
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LabelComponent);
